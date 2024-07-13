00290628a39946d636933da7cafadcd595a1af34
p.habib.eshwar <p.habib.eshwar@accenture.com>
2024-07-14 00:27:02 +0530

    add auth to nginx conf; update nginx install script

--- scripts/nginx_installer.sh
+++ scripts/nginx_installer.sh
@@ -2,20 +2,36 @@
 
 sleep 3s
 
-echo "deb https://nginx.org/packages/ubuntu/ jammy nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
-echo "deb-src https://nginx.org/packages/ubuntu/ jammy nginx" | sudo tee -a /etc/apt/sources.list.d/nginx.list
+echo "================================"
+echo "STARTING NGINX INSTALLATION"
+echo "================================"
 
-key="ABF5BD827BD9BF62"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
+sudo apt update && sudo apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 
-sudo apt update && sudo apt install -y nginx
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 
-sleep 3s
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 
-echo "starting Nginx..."
+# install stable release
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | sudo tee /etc/apt/preferences.d/99nginx
+
+sudo apt update
+sudo apt install nginx
+
+echo "================================"
+echo "STARTING NGINX..."
+echo "================================"
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
 sudo systemctl status nginx
 
-echo "Nginx installation finished!"
+echo "================================"
+echo "NGINX INSTALLATION FINISHED!"
+echo "================================"
